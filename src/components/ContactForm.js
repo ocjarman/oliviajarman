c9b1379200b27d5bfb40a09ea42e1b52c526ea13
ocjarman <ocjarman@gmail.com>
2023-01-10 11:11:18 -0500

    email js service working!

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import { send } from "emailjs-com";
+
 // import axios from "axios";
 import {
   Button,
@@ -14,115 +16,109 @@ const ContactForm = () => {
   //   const dispatch = useDispatch();
   //   const navigate = useNavigate();
 
-  const handleSubmit = async (event) => {
-    console.log("submitted!");
-    //     if (!Object.values(validity).includes(true)) {
-    //       try {
-    //         event.preventDefault();
-    //         const { data: created } = await axios.post("/api/user", userToCreate);
-    //         dispatch(setUser(created));
-    //         navigate("/");
-    //       } catch (error) {
-    //         console.error(error);
-    //       }
+  const [toSend, setToSend] = useState({
+    from_name: "",
+    to_name: "",
+    message: "",
+    reply_to: "",
+  });
+
+  //
+  const handleChange = (e) => {
+    setToSend({ ...toSend, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    send("service_8buah8p", "template_pl8pv7t", toSend, "YOWFCN-XmxUzehOgQ")
+      .then((response) => {
+        console.log("SUCCESS!", response.status, response.text);
+      })
+      .catch((err) => {
+        console.log("FAILED...", err);
+      });
   };
 
   return (
     <Container
-      sx={{ display: "flex", flexDirection: "column", placeSelf: "center" }}
+      sx={{ display: "flex", flexDirection: "column", placeSelf: "flex-start" }}
     >
+      <Typography sx={{ placeSelf: "flex-start" }} variant={"h3"}>
+        Contact
+      </Typography>
+
       <form
-        action="https://gmail.us9.list-manage.com/subscribe/post?u=60b6c2ee8b150eb6bb23c7e53&amp;id=444898526b&amp;f_id=00780ee1f0"
-        method="post"
-        id="mc-embedded-subscribe-form"
-        name="mc-embedded-subscribe-form"
-        class="validate"
-        target="_blank"
-        novalidate
+        id="contact_form"
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          placeSelf: "flex-start",
+        }}
       >
-        <div id="mc_embed_signup_scroll">
-          <h2>Subscribe</h2>
-          <div class="indicates-required">
-            <span class="asterisk">*</span> indicates required
-          </div>
-          <div class="mc-field-group">
-            <label for="mce-EMAIL">
-              Email Address <span class="asterisk">*</span>
-            </label>
-            <input
-              type="email"
-              value=""
-              name="EMAIL"
-              class="required email"
-              id="mce-EMAIL"
-              required
-            />
-            <span id="mce-EMAIL-HELPERTEXT" class="helper_text"></span>
-          </div>
-          <div class="mc-field-group">
-            <label for="mce-FNAME">First Name </label>
-            <input type="text" value="" name="FNAME" class="" id="mce-FNAME" />
-            <span id="mce-FNAME-HELPERTEXT" class="helper_text"></span>
-          </div>
-          <div class="mc-field-group">
-            <label for="mce-LNAME">Last Name </label>
-            <input type="text" value="" name="LNAME" class="" id="mce-LNAME" />
-            <span id="mce-LNAME-HELPERTEXT" class="helper_text"></span>
-          </div>
-          <div class="mc-field-group">
-            <label for="mce-MMERGE6">Message </label>
-            <input
-              type="text"
-              value=""
-              name="MMERGE6"
-              class=""
-              id="mce-MMERGE6"
-            />
-            <span id="mce-MMERGE6-HELPERTEXT" class="helper_text"></span>
-          </div>
-          <div id="mce-responses" class="clear foot">
-            <div
-              class="response"
-              id="mce-error-response"
-              style={{ display: "none" }}
-            ></div>
-            <div
-              class="response"
-              id="mce-success-response"
-              style={{ display: "none" }}
-            ></div>
-          </div>
-          <div
-            style={{ position: "absolute", left: "-5000px;" }}
-            aria-hidden="true"
-          >
-            <input
-              type="text"
-              name="b_60b6c2ee8b150eb6bb23c7e53_444898526b"
-              tabindex="-1"
-              value=""
-            />
-          </div>
-          <div class="optionalParent">
-            <div class="clear foot">
-              <input
-                type="submit"
-                value="Subscribe"
-                name="subscribe"
-                id="mc-embedded-subscribe"
-                class="button"
-              />
-              <p class="brandingLogo">
-                <a
-                  href="http://eepurl.com/ih1S9P"
-                  title="Mailchimp - email marketing made easy and fun"
-                >
-                  <img src="https://eep.io/mc-cdn-images/template_images/branding_logo_text_dark_dtp.svg" />
-                </a>
-              </p>
-            </div>
-          </div>
-        </div>
+        <FormControl required>
+          <InputLabel htmlFor="name-input">Your Name</InputLabel>
+          <Input
+            type="text"
+            name="from_name"
+            value={toSend.from_name}
+            onChange={handleChange}
+          />
+          <FormHelperText id="name-helper-text"></FormHelperText>
+        </FormControl>
+
+        <FormControl required>
+          <InputLabel htmlFor="email-input">Your Email</InputLabel>
+          <Input
+            type="text"
+            name="reply_to"
+            value={toSend.reply_to}
+            onChange={handleChange}
+          />
+          <FormHelperText id="email-helper-text"></FormHelperText>
+        </FormControl>
+        <FormControl>
+          <InputLabel htmlFor="name-input">Company</InputLabel>
+          <Input
+            name="company"
+            id="company"
+            aria-describedby="company-helper-text"
+            onChange={handleChange}
+          />
+          <FormHelperText id="name-helper-text"></FormHelperText>
+        </FormControl>
+
+        <FormControl>
+          <InputLabel htmlFor="phoneNum-input">Your Phone Number</InputLabel>
+          <Input
+            name="phoneNum"
+            id="phoneNum-input"
+            aria-describedby="phoneNum-helper-text"
+            onChange={handleChange}
+          />
+          <FormHelperText id="phoneNum-helper-text">
+            Please enter phone number with area code.
+          </FormHelperText>
+        </FormControl>
+
+        <FormControl required>
+          <InputLabel htmlFor="message-input">Your message</InputLabel>
+          <Input
+            type="text"
+            name="message"
+            value={toSend.message}
+            onChange={handleChange}
+          />
+          <FormHelperText id="message-helper-text"></FormHelperText>
+        </FormControl>
+        <Button
+          size="large"
+          sx={{ width: "10vw", height: "5vh" }}
+          onClick={handleSubmit}
+          variant="contained"
+          type="submit"
+        >
+          Submit
+        </Button>
       </form>
     </Container>
   );
