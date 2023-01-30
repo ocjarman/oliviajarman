b265e25404fbef70ecbec4fe32b3c1be1f975a55
ocjarman <ocjarman@gmail.com>
2023-01-30 10:25:00 -0500

    deleted old tech stack code

--- src/components/TechnicalProficiencies.js
+++ src/components/TechnicalProficiencies.js
@@ -91,49 +91,6 @@ const TechnicalProficiencies = () => {
         </Grid>
       ))}
     </Grid>
-
-    // <Container
-    //   maxWidth={false}
-    //   sx={{
-    //     textAlign: "center",
-    //     marginTop: "5%",
-    //     display: "flex",
-    //     flexDirection: "column",
-    //     flexWrap: "wrap",
-    //   }}
-    // >
-    //   <Typography variant="h5" component="h5">
-    //     TECH STACK
-    //   </Typography>
-
-    //   <Container sx={{ display: "flex", flexDirection: "column" }}>
-    //     <Container>
-    //       <Typography variant="h5" component="h6" sx={{ marginTop: "3%" }}>
-    //         Languages
-    //       </Typography>
-    //       <img src={cssLogo} alt={"css"} height="40vh" />
-    //       <img src={javascript} alt={"js"} height="40vh" />
-    //       <img src={html5} alt={"html"} height="40vh" />
-    //     </Container>
-    //     <Container>
-    //       <Typography variant="h5" component="h6" sx={{ marginTop: "3%" }}>
-    //         Frameworks
-    //       </Typography>
-    //       <img src={reactlogo} alt={"react"} height="40vh" />
-    //       <img src={nodejs} alt={"react"} height="40vh" />
-    //       <img src={firebase} alt={"firebase"} height="40vh" />
-    //       <img src={psql} alt={"firebase"} height="40vh" />
-    //     </Container>
-    //     <Container>
-    //       <Typography variant="h5" component="h6" sx={{ marginTop: "3%" }}>
-    //         Libraries
-    //       </Typography>
-    //       <img src={muilogo} alt={"mui"} height="40vh" />
-    //       <img src={reduxlogo} alt={"redux"} height="40vh" />
-    //       <img src={sequelize} alt={"firebase"} height="40vh" />
-    //     </Container>
-    //   </Container>
-    // </Container>
   );
 };
 
