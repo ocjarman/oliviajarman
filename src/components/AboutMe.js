9e91b2d2b688e64e4d6d22e57ada36d4880c8dab
ocjarman <ocjarman@gmail.com>
2023-02-17 13:44:34 -0500

    update about me section

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -14,16 +14,14 @@ function SimpleDialog(props) {
   return (
     <Dialog onClose={handleClose} open={open}>
       <List sx={{ padding: "10%", width: "80%" }}>
-        Hi! My name is Olivia, and I am an aspiring software engineer graduating
-        from Fullstack Academy of Code in February of 2023.<br></br>
+        Hi! My name is Olivia, and I am a software engineer graduating from
+        Fullstack Academy of Code in February of 2023.<br></br>
         <br></br>
         Prior to my shift into software engineering, I was a science teacher and
         program manager at a college access program with about 7 years of
-        experience in education. My work focused on preparing and coaching high
+        professional experience. My work focused on preparing and coaching high
         school students in the college application processes, in addition to
-        project management for various events and onboarding of new staff
-        members. I also completed 25 credit hours towards becoming a school
-        psychologist at Brooklyn College from 2020-2021. <br></br>
+        project and event management. <br></br>
         <br></br>I found myself drawn to software engineering during my
         maternity leave from graduate school. I started learning to code during
         my son’s nap time, and fell in love with the feeling I got from building
@@ -34,10 +32,10 @@ function SimpleDialog(props) {
         <br></br>
         My strengths include learning new technologies quickly, staying
         organized, and communicating with teammates about project goals,
-        visions, and next steps. I am proud of the projects I have been able to
-        complete solo and with teams at Fullstack Academy with the various new
-        technologies and languages I have learned like React, Redux, and
-        Javascript. <br></br>
+        visions, and next steps. I've completed individual and group project at
+        Fullstack Academy of Code, with various new technologies and languages I
+        have learned like TypeScript, JavaScript, React, and Redux.
+        <br></br>
         <br></br>
         I’m looking forward to exploring fullstack roles where I can be part of
         a team with a lot of mentorship, learn a ton (and eventually teach
