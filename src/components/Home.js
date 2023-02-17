32c8cc46b7230f4a9a375ba970bf2ba3db86e83f
ocjarman <ocjarman@gmail.com>
2023-02-17 12:17:40 -0500

    black animated background working

--- src/components/Home.js
+++ src/components/Home.js
@@ -13,7 +13,9 @@ import { Typography } from "@mui/material";
 import TechnicalProficiencies from "./TechnicalProficiencies";
 import Divider from "@mui/material/Divider";
 import github from "../static/images/github.png";
-
+import Particles from "react-tsparticles";
+import { loadFull } from "tsparticles";
+import { useCallback } from "react";
 import "./home.css";
 
 const theme = createTheme({
@@ -37,78 +39,157 @@ const theme = createTheme({
 });
 
 const Home = () => {
+  const options = {
+    background: {
+      color: "#000000",
+    },
+    interactivity: {
+      events: {
+        onClick: {
+          enable: true,
+          mode: "push",
+        },
+        onHover: {
+          enable: true,
+          mode: "repulse",
+        },
+        resize: true,
+      },
+      modes: {
+        bubble: {
+          distance: 400,
+          duration: 2,
+          opacity: 0.8,
+          size: 40,
+        },
+        push: {
+          quantity: 4,
+        },
+        repulse: {
+          distance: 200,
+          duration: 0.4,
+        },
+      },
+    },
+    particles: {
+      color: {
+        value: "#ffffff",
+      },
+      links: {
+        color: "#ffffff",
+        distance: 150,
+        enable: true,
+        opacity: 0.5,
+        width: 1,
+      },
+      collisions: {
+        enable: true,
+      },
+      move: {
+        direction: "none",
+        enable: true,
+        outMode: "bounce",
+        random: false,
+        speed: 6,
+        straight: false,
+      },
+      number: {
+        density: {
+          enable: true,
+          value_area: 800,
+        },
+        value: 80,
+      },
+      opacity: {
+        value: 0.5,
+      },
+      shape: {
+        type: "star",
+      },
+      size: {
+        random: true,
+        value: 5,
+      },
+    },
+  };
+
+  const particlesInit = useCallback(async (engine) => {
+    await loadFull(engine);
+  }, []);
+
   return (
-    <ThemeProvider theme={theme}>
-      <Container sx={styles.sx.HomeContainer}>
-        <Projects theme={theme} />
-        <Container sx={styles.sx.LogoContainer}>
-          <img
-            className="logo"
-            src={initials}
-            alt={"logo"}
-            width="auto"
-            height="auto"
-            styles={styles.sx.Logo}
-          />
-          <Typography variant="h3" component="h3">
-            OLIVIA JARMAN
-          </Typography>
+    // <ThemeProvider theme={theme}>
+    <Container sx={styles.sx.HomeContainer}>
+      <Particles options={options} init={particlesInit} />
+      <Projects theme={theme} />
+      <Container sx={styles.sx.LogoContainer}>
+        <img
+          className="logo"
+          src={initials}
+          alt={"logo"}
+          width="auto"
+          height="auto"
+          styles={styles.sx.Logo}
+        />
+        <Typography variant="h3" component="h3">
+          OLIVIA JARMAN
+        </Typography>
 
-          <Typography
-            variant="h5"
-            component="h5"
-            sx={{
-              display: "flex",
-              marginTop: "5%",
-              justifyContent: "center",
-            }}
-          >
-            SOFTWARE ENGINEER | FULLSTACK DEVELOPER
-          </Typography>
+        <Typography
+          variant="h5"
+          component="h5"
+          sx={{
+            display: "flex",
+            marginTop: "5%",
+            justifyContent: "center",
+          }}
+        >
+          SOFTWARE ENGINEER | FULLSTACK DEVELOPER
+        </Typography>
 
-          <Container
-            sx={{
-              display: "flex",
-              marginTop: "5%",
-              justifyContent: "center",
-              maxWidth: "500px",
-              alignItems: "center",
-              gap: "1rem",
-            }}
-          >
-            <Button href="https://www.linkedin.com/in/oliviajarman/">
-              <img
-                src={linkedIn}
-                styles={styles.sx.GitHubIcon}
-                alt={"linkedin"}
-                height="70vh"
-              />
-            </Button>
-            <Divider />
-            <Link href="https://www.instagram.com/livcath/?hl=en">
-              <img
-                src={instagramIcon}
-                styles={styles.sx.GitHubIcon}
-                alt={"instagram"}
-                height="78vh"
-              />
-            </Link>
-            <Divider />
-            <Link href="https://github.com/ocjarman">
-              <img
-                src={github}
-                styles={styles.sx.GitHubIcon}
-                alt={"linkedin"}
-                height="80vh"
-              />
-            </Link>
-          </Container>
-          <AboutMe />
+        <Container
+          sx={{
+            display: "flex",
+            marginTop: "5%",
+            justifyContent: "center",
+            maxWidth: "500px",
+            alignItems: "center",
+            gap: "1rem",
+          }}
+        >
+          <Button href="https://www.linkedin.com/in/oliviajarman/">
+            <img
+              src={linkedIn}
+              styles={styles.sx.GitHubIcon}
+              alt={"linkedin"}
+              height="70vh"
+            />
+          </Button>
+          <Divider />
+          <Link href="https://www.instagram.com/livcath/?hl=en">
+            <img
+              src={instagramIcon}
+              styles={styles.sx.GitHubIcon}
+              alt={"instagram"}
+              height="78vh"
+            />
+          </Link>
+          <Divider />
+          <Link href="https://github.com/ocjarman">
+            <img
+              src={github}
+              styles={styles.sx.GitHubIcon}
+              alt={"linkedin"}
+              height="80vh"
+            />
+          </Link>
         </Container>
-        <TechnicalProficiencies theme={theme} />
-        <ContactForm theme={theme} />
+        <AboutMe />
       </Container>
-    </ThemeProvider>
+      <TechnicalProficiencies theme={theme} />
+      <ContactForm theme={theme} />
+    </Container>
+    // </ThemeProvider>
   );
 };
 
