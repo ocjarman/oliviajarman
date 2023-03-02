e714c43696339ae8ff61c4e59a11326fbd576880
ocjarman <ocjarman@gmail.com>
2023-03-01 21:16:14 -0500

    disabled circles on click

--- src/components/Home.js
+++ src/components/Home.js
@@ -47,7 +47,7 @@ const Home = () => {
     interactivity: {
       events: {
         onClick: {
-          enable: true,
+          enable: false,
           mode: "push",
         },
         onHover: {
